0440b16485ef06301e3609d16566adec3ccff6bf
Felipe Soares <felipesb340@hotmail.com>
2024-11-18 22:13:40 -0300

    Primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Atualize para o local dos seus arquivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
